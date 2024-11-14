0d8beb75e3c3d9e0d52a258e467585515185fbf7
Lennard Wolf <wolf@amiconsult.de>
2024-11-14 13:35:04 +0100

    feat: navlinks with current page underlined

---
+++ src/lib/components/NavLinks.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	// store in which page the user is currently on
+	import { page } from '$app/stores';
+	let currentPage: string;
+	function isCurrentPage(href: string) {
+		return currentPage === href;
+	}
+	const links = [
+		{ name: 'Home', href: '/' },
+		{ name: 'About', href: '/about' },
+		{ name: 'Blog', href: '/blog' },
+		{ name: 'organization', href: '/organization' }
+	];
+
+	$: currentPage = $page.url.pathname;
+</script>
+
+{#each links as link}
+	<a href={link.href} class={isCurrentPage(link.href) ? 'active' : ''}>{link.name}</a>
+{/each}
+
+<style>
+	a {
+		color: #000000;
+		text-decoration: none;
+	}
+	a.active {
+		color: #000000;
+		text-decoration: underline;
+	}
+</style>
